86d702ee6dadc94a95f16d3d5aeff671c9da62a5
Nadzeya Petrova <nightmare.evans13@gmail.com>
2022-12-08 11:55:23 +0300

    List Interface Tasks

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -4,12 +4,19 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
+    ListComparator listComparator = new ListComparator();
+
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
+
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        Double aFun = (5 * Math.pow(Double.parseDouble(a), 2) + 3);
+        double bFun = (5 * Math.pow(Double.parseDouble(b), 2) + 3);
+        return aFun.equals(bFun) ? a.compareTo(b) : Double.compare(aFun, bFun);
     }
 }
